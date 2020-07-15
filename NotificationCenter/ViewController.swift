372fb44876dfe4c9c9a0a3ff7d5d5563bfb74cd9
nadarm <ccc7497@gmail.com>
2020-07-15 18:24:06 +0900

    ui setting

--- NotificationCenter/ViewController.swift
+++ NotificationCenter/ViewController.swift
@@ -8,14 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+//  MARK: - UIViewController
 
+final class ViewController: UIViewController {
+
+    // MARK: - UI Properties
+    
+    private let label = UILabel()
+    private let button = UIButton()
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = .cyan
+        self.setLayout()
+        self.bindStyle()
     }
 
+    // MARK: - Functions
+    
+    private func bindStyle() {
+        self.view.backgroundColor = .white
+        
+        label.textColor = .black
+        label.backgroundColor = .yellow
+        label.text = "얍?"
+        
+        button.setTitleColor(.black, for: .normal)
+        button.setTitle("얍!", for: .normal)
+        button.backgroundColor = .white
+        button.layer.borderColor = UIColor.black.cgColor
+        button.layer.borderWidth = 1
+    }
+    
+    private func setLayout() {
+        [label, button].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            self.view.addSubview($0)
+        }
+        
+        
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerYAnchor, constant: -50),
+            label.widthAnchor.constraint(equalToConstant: 100),
+            
+            button.centerXAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerYAnchor, constant: 20),
+            button.widthAnchor.constraint(equalToConstant: 100),
+        ])
+    }
 
 }
 

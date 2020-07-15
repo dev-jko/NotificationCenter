ecd3f08b830b38d2055d978aede7a09228eb0295
nadarm <ccc7497@gmail.com>
2020-07-15 17:41:10 +0900

    init

---
+++ NotificationCenter/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  NotificationCenter
+//
+//  Created by Jaedoo Ko on 2020/07/15.
+//  Copyright © 2020 jko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .cyan
+    }
+
+
+}
+
